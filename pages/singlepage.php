1a0abeb5f4d461d34538658720254907e708f7ee
abol ghana <aghp81@gmail.com>
2023-12-30 08:42:09 +0330

    اتمام طراحی سرفصل دوره ها

--- pages/singlepage.php
+++ pages/singlepage.php
@@ -319,8 +319,36 @@
                         <!-- سرفصل دوره -->
                         <div class="downlod-box mt-5">
                             <div class="download-item">
-                                <span class="title-download-item">معرفی دوره</span>
-                                <span class="time-download-item" style="margin-left: 15px;">۰۰:۳۲:۰۰ </span>
+                                <span class="number-item">۱</span>
+                                <span class="title-download-item">معرفی دوره</span><br class="d-flex d-lg-none">
+                                <span class="time-download-item">۰۰:۳۲:۰۰ </span>
+                                <span class="status-download-item">رایگانــ</span>
+
+                                <!-- آیکون های ذخیره و اجرا -->
+                                <span class="green-logo"><svg width="1em" height="1em" viewBox="0 0 16 16"
+                                    class="bi bi-cloud-arrow-down-fill" fill="currentColor"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z" />
+                            </svg>
+                        </span>
+
+                            <span class="blue-logo">
+                                <svg width="1em" height="1em" viewBox="0 0 16 16"
+                                    class="bi bi-play-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+                            </svg>
+                        </span>
+                            
+                            <span class="light-logo"><svg width="1em" height="1em" viewBox="0 0 16 16"
+                                    class="bi bi-file-earmark-text-fill" fill="currentColor"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zM4.5 8a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z" />
+                            </svg>
+                        </span>
+                            <!-- آیکون های ذخیره و اجرا -->
                             </div>
                         </div>
                         <!-- سرفصل دوره -->
