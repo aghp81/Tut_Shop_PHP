09722d86e180d950a7c1d7a28a250eab846723cd
aghp81 <aghp81@gmail.com>
2024-01-18 21:59:08 +0330

    Update user.sql

--- user.sql
+++ user.sql
@@ -29,13 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `user-name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `phone` int(11) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL DEFAULT 1,
-  `status` int(11) NOT NULL DEFAULT 1
+  `status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -61,4 +60,5 @@ COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
