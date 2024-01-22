f69e95ca2102abbac7bd7933df8a98a120b8af96
aghp81 <aghp81@gmail.com>
2024-01-22 02:43:32 -0800

    edit SQL

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 22, 2024 at 10:29 AM
+-- Generation Time: Jan 22, 2024 at 11:42 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,14 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL,
-  `user-name` varchar(255) NOT NULL,
-  `phone` int(11) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `phone` varchar(11) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL DEFAULT 1,
   `status` int(11) NOT NULL DEFAULT 0,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `phone`, `email`, `password`, `role`, `status`, `active`) VALUES
+(1, 'aghp81', '917891546', 'aghp81@gmail.com', '123', 1, 0, 3718),
+(2, 'aghp81', '2147483647', 'aghp81@gmail.com', '123', 1, 0, 9989),
+(3, 'aghp81', '09177516245', 'aghp81@gmail.com', '123', 1, 0, 8403);
 
 --
 -- Indexes for dumped tables
@@ -56,7 +65,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
