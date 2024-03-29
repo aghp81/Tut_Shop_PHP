09df7685313bfa7a1893d6e8b6f76e848ca4cf22
abol ghana <aghp81@gmail.com>
2024-03-29 10:30:13 +0330

    افزودن صفحه منو

---
+++ admin/menu.php
@@ -0,0 +1,26 @@
+<?php  include "header.php"  ?>
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+                        <!-- end row -->
+                        <div class="row">
+                            <div class="card-box">
+                                صفحه منو
+                            </div>
+                        </div>
+                        <!-- end row -->
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+
+<?php  include "footer.php";   ?>
