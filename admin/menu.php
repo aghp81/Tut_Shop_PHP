db7c06fbfc4f33ba7dbc058eeccf7276bc2cce25
abol ghana <aghp81@gmail.com>
2024-03-29 10:45:39 +0330

    اصلاح منو

--- admin/menu.php
+++ admin/menu.php
@@ -1,10 +1,6 @@
 <?php  include "header.php"  ?>
 
 
-
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
             <div class="content-page">
                 <!-- Start content -->
                 <div class="content">
