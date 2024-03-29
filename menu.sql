354f219d31c1280ed9ee3a224ee61618b109d526
abol ghana <aghp81@gmail.com>
2024-03-29 17:09:17 +0330

    افزودن ستون src برای آدرس لینک منو

--- menu.sql
+++ menu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2024 at 08:14 AM
+-- Generation Time: Mar 29, 2024 at 02:38 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -31,8 +31,22 @@ CREATE TABLE `menu` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `z` int(11) NOT NULL,
-  `sort` int(11) NOT NULL
+  `sort` int(11) NOT NULL DEFAULT 0,
+  `src` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `title`, `z`, `sort`, `src`) VALUES
+(3, 'پایتون', 0, 1, ''),
+(4, 'php', 0, 6, ''),
+(5, 'لاراول', 4, 2, ''),
+(7, 'جنگو', 3, 8, ''),
+(8, 'برنامه نویسی', 0, 2, ''),
+(9, 'گلسازی', 0, 2, ''),
+(10, 'blade', 5, 1, '');
 
 --
 -- Indexes for dumped tables
@@ -52,7 +66,7 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
