f7f819510ca584d6fe671aaf2f364f5208993871
abol ghana <aghp81@gmail.com>
2024-04-11 11:41:00 +0330

    حذف دوره

--- admin/deletecourse.php
+++ admin/deletecourse.php
@@ -1,8 +1,10 @@
 <?php
 include "../database/db.php";
 
+$id = $_GET['id'];
+
 $result = $conn->prepare('DELETE FROM course WHERE id=?');
-$result->bindValue(1, $_GET['id']);
+$result->bindValue(1, $id);
 $result->execute();
 
 echo '<script>alert ("دوره با موفقیت حذف شد");
